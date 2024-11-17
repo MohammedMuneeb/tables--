6b89d73ad2149374cd6c690c1e596003b2f48f92
MohammedMuneeb <mohammedmuneeb33@gmail.com>
2024-11-17 10:34:11 +0530

    upgrading validations

--- src/DynamicTable.js
+++ src/DynamicTable.js
@@ -421,12 +421,12 @@ function AppendingForm (){
           placeholder ="Description" 
           //   class ="... invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-700" 
         //   className ={`border ${formErrors.description ? 'border-red-500':""}`} 
-          required 
+        //   required 
           /> 
           
             </div > 
-          <div className =""><input type ="text" name ="author"placeholder ="Author "/></div > 
-          <div className =""><input type ="text" name ="generes "placeholder ="Genres "/></div > 
+          <div className =""><input type ="text" name ="author"placeholder ="Author "required /></div > 
+          <div className =""><input type ="text" name =""placeholder ="Genres "/></div > 
           
           {/* <button onClick ={()=>setIsDialogOpen (false )}>Exit </button >  */} 
           
@@ -440,7 +440,7 @@ function AppendingForm (){
           className ="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> 
             Add Book </button > 
             </div > 
-          </form >
+          </form > 
           
         </div > 
         </div > 
